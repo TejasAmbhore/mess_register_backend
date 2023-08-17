8a2d481672abef56e0515f36bf4b52147cbe5daf
Tejas <tejas@timelyai.com>
2023-08-18 01:59:52 +0530

    get request for analysis

--- checkin/views.py
+++ checkin/views.py
@@ -5,7 +5,8 @@ from .models import *
 from .serializers import *
 import datetime
 from django.db import IntegrityError
-
+# from django.db.models import Q
+from datetime import timedelta
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('rollNo')
@@ -14,7 +15,7 @@ class UserViewSet(viewsets.ModelViewSet):
 def get_slot():
     now = datetime.datetime.now().time()
     print(now)
-    breakfast_start = datetime.time(0, 0)
+    breakfast_start = datetime.time(6, 0)
     breakfast_end = datetime.time(10, 0)
     lunch_start = datetime.time(12, 0)
     lunch_end = datetime.time(14, 30)
@@ -55,3 +56,27 @@ class CheckInViewSet(viewsets.ViewSet):
                 return Response({'error': 'Already checked-in for this slot'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def list(self, request):
+        queryset = CheckIn.objects.all()
+        
+        date = request.query_params.get('date', None)
+        last_30_days = request.query_params.get('last_30_days', None)
+        slot = request.query_params.get('slot', None)
+        rollNo = request.query_params.get('rollNo', None)
+
+        if date:
+            queryset = queryset.filter(date=date)
+        elif last_30_days:
+            end_date = datetime.datetime.now().date()
+            start_date = end_date - timedelta(days=30)
+            queryset = queryset.filter(date__range=(start_date, end_date))
+        
+        if slot:
+            queryset = queryset.filter(slot=slot)
+        
+        if rollNo:
+            queryset = queryset.filter(rollNo=rollNo)
+
+        serializer = CheckInSerializer(queryset, many=True)
+        return Response(serializer.data)
